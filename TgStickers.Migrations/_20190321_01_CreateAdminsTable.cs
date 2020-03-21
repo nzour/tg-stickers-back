acf1a0510225e107c241c1db664bb1d4676387d3
zaur <zaur.nadiraliev.228@gmail.com>
2020-03-21 15:22:07 +0300

    Заменил миграцию создателя таблицы Users на миграцию создания Admins

--- TgStickers.Migrations/_20190321_01_CreateAdminsTable.cs
+++ TgStickers.Migrations/_20190321_01_CreateAdminsTable.cs
@@ -3,21 +3,29 @@ using FluentMigrator;
 namespace TgStickers.Migrations
 {
     [Migration(2019032101)]
-    public class _20190321_01_CreateUsersTable : Migration
+    public class _20190321_01_CreateAdminsTable : Migration
     {
         public override void Up()
         {
-            Create.Table("Users")
+            Create.Table("Admins")
                 .WithColumn("Id").AsGuid().PrimaryKey()
                 .WithColumn("Name").AsString().NotNullable()
                 .WithColumn("Login").AsString().NotNullable()
                 .WithColumn("Password").AsString().NotNullable()
-                .WithColumn("Role").AsString().NotNullable();
+                .WithColumn("CreatedAt").AsDateTime().NotNullable();
+
+            Create.UniqueConstraint("Admins_Login_key")
+                .OnTable("Admins")
+                .Column("Login");
+
+            Create.Index("Admins_Login_ids")
+                .OnTable("Admins")
+                .OnColumn("Login");
         }
 
         public override void Down()
         {
-            Delete.Table("Users");
+            Delete.Table("Admins");
         }
     }
 }
