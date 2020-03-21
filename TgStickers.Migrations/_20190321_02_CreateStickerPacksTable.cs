074494620552990225c8f1a084473d17e19ca87a
zaur <zaur.nadiraliev.228@gmail.com>
2020-03-21 15:46:22 +0300

    Добавил миграцию для создания таблицы StickerPacks

---
+++ TgStickers.Migrations/_20190321_02_CreateStickerPacksTable.cs
@@ -0,0 +1,35 @@
+using FluentMigrator;
+using static System.Data.Rule;
+
+namespace TgStickers.Migrations
+{
+    [Migration(2019032102)]
+    public class _20190321_02_CreateStickerPacksTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("StickerPacks")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("SharedUrl").AsString().NotNullable()
+                .WithColumn("CreatedAt").AsDateTime().NotNullable()
+                .WithColumn("CreatedById").AsGuid().NotNullable();
+
+            Create.Index("StickerPacks_Name_idx")
+                .OnTable("StickerPacks")
+                .OnColumn("Name");
+
+            Create.ForeignKey("StickerPacks_CreatedById_fkey")
+                .FromTable("StickerPacks")
+                .ForeignColumn("CreatedById")
+                .ToTable("Admins")
+                .PrimaryColumn("Id")
+                .OnDelete(Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("StickerPacks");
+        }
+    }
+}
