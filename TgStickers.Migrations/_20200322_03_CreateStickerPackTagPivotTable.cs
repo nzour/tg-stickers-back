1b032dbbf2406fc541c869153d5aa4e5aa450086
zaur <zaur.nadiraliev.228@gmail.com>
2020-03-22 15:38:27 +0300

    Миграция для создания таблицы StickerTags_Pivot

---
+++ TgStickers.Migrations/_20200322_03_CreateStickerPackTagPivotTable.cs
@@ -0,0 +1,36 @@
+using FluentMigrator;
+
+namespace TgStickers.Migrations
+{
+    [Migration(2020032203)]
+    public class _20200322_03_CreateStickerPackTagPivotTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("StickerTags_Pivot")
+                .WithColumn("StickerPackId").AsGuid().NotNullable()
+                .WithColumn("TagId").AsGuid().NotNullable();
+
+            Create.PrimaryKey()
+                .OnTable("StickerTags_Pivot")
+                .Columns("StickerPackId", "TagId");
+
+            Create.ForeignKey("StickerTags_Pivot_StickerPackId_fkey")
+                .FromTable("StickerTags_Pivot")
+                .ForeignColumn("StickerPackId")
+                .ToTable("StickerPacks")
+                .PrimaryColumn("Id");
+
+            Create.ForeignKey("StickerTags_Pivot_TagId_fkey")
+                .FromTable("StickerTags_Pivot")
+                .ForeignColumn("TagId")
+                .ToTable("Tags")
+                .PrimaryColumn("Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("StickerTags_Pivot");
+        }
+    }
+}
